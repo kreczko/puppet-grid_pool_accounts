e12765a56405c27a98f5178fb4ca3ee9a1801bb6
kreczko <lkreczko@gmail.com>
2013-07-11 12:43:41 +0100

    first feature set for grid pool account

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,6 +10,36 @@
 #
 # Sample Usage:
 #
-class grid_pool_accounts {
+define grid_pool_accounts (
+  $account_prefix          = $title,
+  $account_number_start    = "000",
+  $account_number_end      = "001",
+  $user_ID_number_start    = 90000,
+  $user_ID_number_end      = 90001,
+  $primary_group           = undef,
+  $groups                  = [
+    ],
+  $comment                 = "mapped user for group $title",
+  $create_home_dir         = true,
+  $create_gridmapdir_entry = false) {
+  $users     = range("${account_prefix}${account_number_start}", "${account_prefix}${account_number_end}")
+  $uids      = range("${user_ID_number_start}", "${user_ID_number_end}")
+
+  $uid_size  = size($uids)
+  $user_size = size($users)
+
+  if $uid_size == $user_size {
+    pool_account{$users:
+      manage_home => $create_home_dir,
+      primary_group => $primary_group,
+      groups => $groups,
+      uid => $uids,
+      create_gridmapdir_entry => $create_gridmapdir_entry,
+    }
+  } else {
+    notify { "grid_pool_accounts_error": 
+      message => "UID range is not the same as account range. UID range :$user_ID_number_start - $user_ID_number_end, account range: $account_number_start - $account_number_end",
+    }
+  }
 
 }
