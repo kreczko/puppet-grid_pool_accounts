28e04e0216131727e70d88c93e5382f04aad7cba
kreczko <lkreczko@gmail.com>
2013-07-11 12:44:17 +0100

    adding skeleton for grid roles

---
+++ manifests/add_grid_role.pp
@@ -0,0 +1,9 @@
+define add_grid_role ($role = $title,
+  $user = undef,
+  $group = undef,
+){
+   #update /etc/grid-security/grid-mapfile
+   #group -> role .group
+   #user -> role user
+   #use augeas lense
+}
