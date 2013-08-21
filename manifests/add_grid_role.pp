498208337e7e9f3fda149dc7a9f51ac9f94378d5
Luke Kreczko <lkreczko@gmail.com>
2013-08-21 10:37:40 +0100

    adding preliminary defines and more notes on grid roles

--- manifests/add_grid_role.pp
+++ manifests/add_grid_role.pp
@@ -3,6 +3,7 @@ define add_grid_role ($role = $title,
   $group = undef,
 ){
    #update /etc/grid-security/grid-mapfile
+   #actually, this should go into /etc/edg-mkgridmap.conf and make sure to run the command (edg-mk..)
    #group -> role .group
    #user -> role user
    #use augeas lense
